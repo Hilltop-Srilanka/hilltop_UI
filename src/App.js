9670240853f6ebdc1a8925f5661209e9ca2451fd
nexzanavod <aanavod@gmail.com>
2023-09-29 22:39:06 +0530

    Add tithes

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component, Suspense } from 'react'
+import { HashRouter, Route, Routes } from 'react-router-dom'
+import './scss/style.scss'
+
+const loading = (
+  <div className="pt-3 text-center">
+    <div className="sk-spinner sk-spinner-pulse"></div>
+  </div>
+)
+
+// Containers
+const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
+
+// Pages
+const Login = React.lazy(() => import('./views/pages/login/Login'))
+const Register = React.lazy(() => import('./views/pages/register/Register'))
+const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
+const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
+const OrganizersPage = React.lazy(() => import('./views/pages/main/orginizers/OrganizersPage'))
+const VotersPage = React.lazy(() => import('./views/pages/main/voters/VotersPage'))
+const Income = React.lazy(() => import('./views/pages/main/Income/AddIncome'))
+
+
+class App extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <Suspense fallback={loading}>
+          <Routes>
+            <Route exact path="/login" name="Login Page" element={<Login />} />
+            <Route exact path="/register" name="Register Page" element={<Register />} />
+            <Route exact path="/#/organizers" name="Login Page" element={<OrganizersPage />} />
+            <Route exact path="/#/voters" name="Login Page" element={<VotersPage />} />
+            <Route exact path="/#/income" name="Income Page" element={<Income />} />
+            <Route exact path="/404" name="Page 404" element={<Page404 />} />
+            <Route exact path="/500" name="Page 500" element={<Page500 />} />
+            <Route path="*" name="Home" element={<DefaultLayout />} />
+          </Routes>
+        </Suspense>
+      </HashRouter>
+    )
+  }
+}
+
+export default App
